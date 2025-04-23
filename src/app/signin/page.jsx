373b10ce73a258db3dd7174cc5bf9515aaf32f40
rakaso598 <112613372+rakaso598@users.noreply.github.com>
2025-04-23 20:18:09 +0900

    로그인&회원가입 응답로직 작성

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import UserEmailInput from '../components/UserEmailInput'
 import UserPasswordInput from '../components/UserPasswordInput'
 import SubmitButton from '../components/SubmitButton'
 
 function SigninPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      //요청
+      const response = await fetch('https://panda-market-api.vercel.app/auth/signIn', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "abababcd1@email.com",
+          password: "abababcd",
+        }),
+      });
+      //응답
+
+      const data = await response.json();
+      alert(data.user.nickname);
+
+      // 일반 쿠키에 저장 (XSS 취약점 주의)
+      document.cookie = `accessToken=${data.accessToken}; path=/; max-age=${60 * 60}; secure:false; httpOnly`;
+      document.cookie = `refreshToken=${data.refreshToken}; path=/; max-age=${60 * 60 * 24 * 7}; secure:false; httpOnly`;
+
+    } catch (error) {
+      console.error('로그인 중 오류:', error)
+    }
+  }
+
   return (
-    <form>
-      <UserEmailInput />
-      <UserPasswordInput />
+    <form onSubmit={handleSubmit}>
+      <UserEmailInput value={email} onChange={(e) => setEmail(e.target.value)} />
+      <UserPasswordInput value={password} onChange={(e) => setPassword(e.target.value)} />
       <SubmitButton label={"로그인"} />
     </form>
   )
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -9,26 +9,27 @@ function SignupPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
     try {
       //요청
-      const response = fetch('https://panda-market-api.vercel.app/auth/signUp', {
+      const response = await fetch('https://panda-market-api.vercel.app/auth/signUp', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          email: "abababcd1@email.com",
-          nickname: "abababcd1",
+          email: "abababcd2@email.com",
+          nickname: "abababcd2",
           password: "abababcd",
           passwordConfirmation: "abababcd"
         }),
       });
       //응답
+
       const data = await response.json();
-      console.log('회원가입 성공:', data);
+      alert(data.user.nickname);
 
     } catch (error) {
       console.error('회원가입 요청 중 오류:', error)
